de668caa918cab71361e45a21bf3cb2becba30ff
Nama Kamu <email@kamu.com>
2024-11-26 09:21:06 +0700

    first commit

---
+++ resources/views/component/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="bg-body-tertiary text-center text-lg-start">
+    <div class="text-center p-3">
+        © 2020 Copyright
+        <a href="">-Inaklug Indonesia | Hak cipta dilindungi undang-undang</a>
+    </div>
+</footer>
